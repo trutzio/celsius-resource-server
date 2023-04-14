452f9ae909dcc710d8e7593ccc606ebfebaf1b81
Christian Trutz <info@trutz-software-consulting.de>
2023-04-14 16:56:37 +0200

    feat: Spring Boot Projekt erstellt

---
+++ src/main/java/trutz/io/celsiusresourceserver/CelsiusResourceServerApplication.java
@@ -0,0 +1,13 @@
+package trutz.io.celsiusresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelsiusResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelsiusResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trutz/io/celsiusresourceserver/CelsiusResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package trutz.io.celsiusresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelsiusResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
